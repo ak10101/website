16be4456c1e034fdbcfa99b51819fcd1157ac1a8
AKHIL_PAVAL <akhile3226@gmail.com>
2022-03-24 16:20:11 +0530

    Updated Website

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        'merry': ['Merriweather',  ...defaultTheme.fontFamily.serif],
+        'custom': ['Grand Hotel',  ...defaultTheme.fontFamily.serif]
+
+      },
+    },
+  },
+  plugins: [],
+}
