90b6258aea69086509412083d205974e78a0db9e
Akhil <akhile3226@gmail.com>
2022-05-23 17:08:28 +0530

    fix

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,33 @@
 import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
-import Link from 'next/link'
+import Link from "next/link";
 
 export default function Home() {
-
   return (
     <div className="min-h-screen bg-gray-50 py-6 flex flex-col justify-center relative overflow-hidden sm:py-12">
-      
+      <Head>
+        <title>Corner Cafe - Drink the delight</title>
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
+      </Head>
+
       <div className="absolute inset-0 bg-[url(/img/grid.svg)] bg-center [mask-image:linear-gradient(180deg,white,rgba(255,255,255,0))]"></div>
       <div className="relative px-6 pt-10 pb-8 bg-white shadow-xl ring-1 ring-gray-900/5 sm:max-w-lg sm:mx-auto sm:rounded-lg sm:px-10">
         <div className="max-w-md mx-auto">
@@ -24,7 +44,7 @@ export default function Home() {
 
           <div className="mt-3 font-merry text-md sm:text-xl">
             <p>Are you tired of getting coffee late?</p>
-            <p>We have the solution</p>
+            <p>Not anymore!</p>
           </div>
 
           <div className="my-2 font-merry font-bold text-md sm:text-lg text-yellow-900">
@@ -37,7 +57,7 @@ export default function Home() {
           <div className="divide-y divide-gray-300/50"></div>
           <div className="pt-8 text-base font-merry leading-7 font-semibold">
             <div>
-             <p className="text-xl border-b my-2">Our team</p>   
+              <p className="text-xl border-b my-2">Our team</p>
             </div>
             <ul className="flex justify-center">
               <li className="flex items-center rounded-lg p-2">
@@ -46,8 +66,12 @@ export default function Home() {
                   src="https://avatars.githubusercontent.com/u/102219762?v=4"
                 />
                 <div className="mx-2">
-                  <h6 className="text-sm sm:text-lg leading-3 font-semibold">Akhil E</h6>
-                  <p className="text-gray-500 text-xs sm:text-sm font-medium">Founder</p>
+                  <h6 className="text-sm sm:text-lg leading-3 font-semibold">
+                    Akhil E
+                  </h6>
+                  <p className="text-gray-500 text-xs sm:text-sm font-medium">
+                    Founder
+                  </p>
                 </div>
               </li>
               <li className="flex items-center rounded-lg p-2">
@@ -56,15 +80,23 @@ export default function Home() {
                   src="https://avatars.githubusercontent.com/u/101086460?v=4"
                 />
                 <div className="mx-2">
-                  <h6 className="text-sm sm:text-lg leading-3 font-semibold">SD Paval</h6>
-                  <p className="text-gray-500 text-xs sm:text-sm font-medium">Founder</p>
+                  <h6 className="text-sm sm:text-lg leading-3 font-semibold">
+                    SD Paval
+                  </h6>
+                  <p className="text-gray-500 text-xs sm:text-sm font-medium">
+                    Founder
+                  </p>
                 </div>
               </li>
             </ul>
           </div>
-          <div className="flex items-center text-xs my-3 space-x-3">
-          <Link href="/tc"><a className="mx-2">Terms and Conditions</a></Link>
-          <Link href="/rc"><a className="mx-2">Return and Cancellation Policy</a></Link>
+          <div className="flex items-center justify-center border-t-2 pt-4 border-amber-700 border-opacity-60 text-xs my-3 space-x-3">
+            <Link href="/tc">
+              <a className="mx-2  hover:text-blue-800 hover:underline">Terms and Conditions</a>
+            </Link>
+            <Link href="/rc">
+              <a className="mx-2  hover:text-blue-800 hover:underline">Return and Cancellation Policy</a>
+            </Link>
           </div>
         </div>
       </div>
--- pages/rc.js
+++ pages/rc.js
@@ -3,15 +3,41 @@ import Image from "next/image";
 import styles from "../styles/Home.module.css";
 
 export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Return and Cancellation Policy - Corner Cafe</title>
+      </Head>
+      <section className="mx-auto container w-full my-4">
+        <article className="prose">
+          <h2>Refund and Cancellation Policy</h2>
+          <p>
+            Our focus is complete customer satisfaction. In the event, if you
+            are displeased with the services provided, we will refund back the
+            money, provided the reasons are genuine and proved after
+            investigation. Please keep the transaction id, it provides all the
+            details about the services or the product you purchase.
+          </p>
+          <h3>Cancellation Policy</h3>
+          <p>
+            For Cancellations please contact us via email (
+            <a href="mailto:cornercafe.shop@gmail.com">
+              cornercafe.shop@gmail.com
+            </a>
+            ) Requests received later than 3 to 4 business days prior to the end
+            of the current service period will be treated as cancellation of
+            services for the next service period.
+          </p>
+          <h3>Refund Policy</h3>
 
-    return (
-        <div>
-            <Head><title>Return and Cancellation Policy</title></Head>
-            <section>
-                <article className="prose lg:prose-xl">
-
-                </article>
-            </section>
-        </div>
-    )
+          <p>
+            In case any client is not get the beverage we can provide a refund.
+            Refunds will be issued to the upi account provided at the time of
+            purchase and in case of payment gateway fails, payments refund will be
+            made to the same account.
+          </p>
+        </article>
+      </section>
+    </div>
+  );
 }
